d5344cff1704411a5036525827dc16c4102a555a
Celeb <qzhou0706@gmail.com>
2025-12-23 14:19:53 +0800

    Fix: Ascii Art

--- lua/plugins/alpha.lua
+++ lua/plugins/alpha.lua
@@ -7,14 +7,25 @@ return {
     local dashboard = require("alpha.themes.dashboard")
     local logo = require("global.ui.ascii_arts").Tokamak.original
     dashboard.section.header.val = vim.split(logo, "\n")
+    -- dashboard.section.buttons.val = {
+    --   dashboard.button("f", " " .. " Find Things", "<cmd>Telescope<cr>"),
+    --   dashboard.button("r", " " .. " Recent Files", "<cmd>Telescope oldfiles<cr>"),
+    --   dashboard.button("c", " " .. " Config", function()
+    --     require("telescope.builtin").find_files({ cwd = vim.fn.stdpath("config") })
+    --   end),
+    --   dashboard.button("l", " " .. " Lazy", "<cmd>Lazy<cr>"),
+    --   dashboard.button("q", " " .. " Quit", "<cmd>qa<cr>"),
+    -- }
     dashboard.section.buttons.val = {
-      dashboard.button("f", " " .. " Find Things", "<cmd>Telescope<cr>"),
-      dashboard.button("r", " " .. " Recent Files", "<cmd>Telescope oldfiles<cr>"),
-      dashboard.button("c", " " .. " Config", function()
-        require("telescope.builtin").find_files({ cwd = vim.fn.stdpath("config") })
-      end),
-      dashboard.button("l", " " .. " Lazy", "<cmd>Lazy<cr>"),
-      dashboard.button("q", " " .. " Quit", "<cmd>qa<cr>"),
+      dashboard.button("f", " " .. " Find file", "<cmd> lua LazyVim.pick()() <cr>"),
+      dashboard.button("n", " " .. " New file", [[<cmd> ene <BAR> startinsert <cr>]]),
+      dashboard.button("r", " " .. " Recent files", [[<cmd> lua LazyVim.pick("oldfiles")() <cr>]]),
+      dashboard.button("g", " " .. " Find text", [[<cmd> lua LazyVim.pick("live_grep")() <cr>]]),
+      dashboard.button("c", " " .. " Config", "<cmd> lua LazyVim.pick.config_files()() <cr>"),
+      dashboard.button("s", " " .. " Restore Session", [[<cmd> lua require("persistence").load() <cr>]]),
+      dashboard.button("x", " " .. " Lazy Extras", "<cmd> LazyExtras <cr>"),
+      dashboard.button("l", "󰒲 " .. " Lazy", "<cmd> Lazy <cr>"),
+      dashboard.button("q", " " .. " Quit", "<cmd> qa <cr>"),
     }
     vim.cmd([[ highlight AlphaLogo guifg=#30D7FF ]])
     vim.cmd([[ highlight AlphaText guifg=#1FB7E0 ]])
